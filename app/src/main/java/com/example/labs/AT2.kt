44c40eb9ea0230ba6d22bab51aab42d620ae4db6
luismonte000 <106330561+luismonte000@users.noreply.github.com>
2024-03-07 15:33:46 +0000

    first commit

---
+++ app/src/main/java/com/example/labs/AT2.kt
@@ -0,0 +1,11 @@
+package com.example.labs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AT2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_at2)
+    }
+}
