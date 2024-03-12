4d3a4207c1017be487301a92c8d89f9bc57aff60
luismonte000 <106330561+luismonte000@users.noreply.github.com>
2024-03-12 15:15:09 +0000

    first commit

--- app/src/main/java/com/example/labs/MainActivity2.kt
+++ app/src/main/java/com/example/labs/MainActivity2.kt
@@ -3,9 +3,9 @@ package com.example.labs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Activity1 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_1)
+        setContentView(R.layout.activity_main2)
     }
 }
