1b70565f8ac6ae25307cf7422c890f83d3f79007
luismonte000 <106330561+luismonte000@users.noreply.github.com>
2024-03-13 12:10:55 +0000

    first commit

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.labs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity2::class.java).apply { putExtra("name", "Atividade 2") }
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/example/labs/MainActivity2.kt
+++ app/src/main/java/com/example/labs/MainActivity2.kt
@@ -2,10 +2,22 @@ package com.example.labs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var buttonClick = findViewById<Button>(R.id.buttonGoToAT1)
+
+        buttonClick.setOnClickListener {
+            finish()
+        }
+
+
+        var name = intent.getStringExtra("name");
+        findViewById<TextView>(R.id.textViewPageId).apply { text = name }
     }
 }
